d27d9a19d30fd235bf289ed1be86e9b8cd87e89d
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-05-04 17:11:04 +0800

    Rcp 10 - System Access (#5)
    * Added login.html template
    
    * sql
    
    * Updated login page layout

--- recipe-create-user-script.sql
+++ recipe-create-user-script.sql
@@ -1,11 +1,11 @@
-CREATE USER recipe IDENTIFIED BY recipe 
-GRANT CONNECT TO recipe 
-GRANT CONNECT, RESOURCE, DBA TO recipe 
-GRANT create session TO recipe 
-GRANT create table TO recipe 
-GRANT create view TO recipe 
-GRANT create any trigger TO recipe 
-GRANT create any procedure TO recipe 
-GRANT create sequence TO recipe 
-GRANT create synonym TO recipe 
-GRANT UNLIMITED TABLESPACE TO recipe 
+CREATE USER recipe IDENTIFIED BY recipe; 
+GRANT CONNECT TO recipe; 
+GRANT CONNECT, RESOURCE, DBA TO recipe; 
+GRANT create session TO recipe; 
+GRANT create table TO recipe; 
+GRANT create view TO recipe; 
+GRANT create any trigger TO recipe; 
+GRANT create any procedure TO recipe; 
+GRANT create sequence TO recipe; 
+GRANT create synonym TO recipe; 
+GRANT UNLIMITED TABLESPACE TO recipe; 
