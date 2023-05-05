5b60909d445071803b1b1c1123bd9c76b2f36b8c
markbugarin <markbgrn@gmail.com>
2023-05-05 14:32:55 +0800

    initial commit

--- src/main/java/com/groupone/recipeappbackend/users/repository/UsersDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.groupone.recipeappbackend.users.repository;
-
-import com.groupone.recipeappbackend.users.model.UsersDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersDetailsRepository extends JpaRepository<UsersDetails,Long> {
-}
