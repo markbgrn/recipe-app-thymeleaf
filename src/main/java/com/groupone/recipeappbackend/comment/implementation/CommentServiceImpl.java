d086ba19b4e8c28fa9e852c0ffdee361f69d10f5
Queenie <queeniemickaela@gmail.com>
2023-05-05 16:35:10 +0800

    RCP-19 As a Registered User, I want to add a comment in a recipe so that I can share my thoughts on it.

--- src/main/java/com/groupone/recipeappbackend/comment/implementation/CommentServiceImpl.java
+++ src/main/java/com/groupone/recipeappbackend/comment/implementation/CommentServiceImpl.java
@@ -6,13 +6,21 @@ import com.groupone.recipeappbackend.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
+
+
     private final CommentRepository commentRepository;
 
+   /* public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }*/
     @Override
     public Comment saveComment(Comment comment) {
+
         return commentRepository.save(comment);
     }
 }
